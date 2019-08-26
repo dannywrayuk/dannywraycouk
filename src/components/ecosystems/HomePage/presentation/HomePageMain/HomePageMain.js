4c1c4bd39392a8014e246a75530965abf8f2ee2d
dannywrayuk <dannywray97@gmail.com>
2019-08-27 00:32:09 +0100

    Added react-helmet and changed entry location

--- src/components/ecosystems/HomePage/presentation/HomePageMain/HomePageMain.js
+++ src/components/ecosystems/HomePage/presentation/HomePageMain/HomePageMain.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 const HomePageMain = () => (
-  <p>hello</p>
+  <div>
+    <Helmet>
+      <title>Home | Danny Wray</title>
+    </Helmet>
+    <p>hello</p>
+  </div>
 );
 
 export default HomePageMain;
--- src/entry/index.js
+++ src/entry/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import './index.css';
-import App from './components/environments/App';
+import App from '../components/environments/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
