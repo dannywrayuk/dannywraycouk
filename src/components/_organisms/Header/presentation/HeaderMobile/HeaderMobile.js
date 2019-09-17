a8422101b9e507f36651c79806d4bfda6d95b2fb
dannywrayuk <dannywray97@gmail.com>
2019-09-17 01:53:57 +0100

    chrome underline issue

--- src/components/_organisms/Header/presentation/HeaderMobile/HeaderMobile.js
+++ src/components/_organisms/Header/presentation/HeaderMobile/HeaderMobile.js
@@ -40,7 +40,7 @@ const HeaderMobile = () => {
       <div className={(searchExtended || menuExtended) ? style.logoHide : style.logoContainer}>
         <div className={style.alignLogo}>
           <LinkButton to={ROUTES.BASE}>
-            <Logo />
+            <Logo showText />
           </LinkButton>
         </div>
       </div>
