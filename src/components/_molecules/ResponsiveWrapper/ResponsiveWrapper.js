33cc2b46cae211b5a45d27ed90d124e5c63a0515
dannywrayuk <dannywray97@gmail.com>
2019-09-17 01:36:54 +0100

    responsive wrapper

---
+++ src/components/_molecules/ResponsiveWrapper/ResponsiveWrapper.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import style from './ResponsiveWrapper.css';
+
+const ResponsiveWrapper = ({ children }) => (
+  <div className={style.fillContainer}>
+    <div className={style.Wrapper}>
+      {children}
+    </div>
+  </div>
+);
+
+ResponsiveWrapper.propTypes = {
+  children: PropTypes.node,
+};
+
+ResponsiveWrapper.defaultProps = {
+  children: null,
+};
+
+export default ResponsiveWrapper;
--- src/components/ecosystems/HomePage/presentation/HomePageMain/HomePageMain.js
+++ src/components/ecosystems/HomePage/presentation/HomePageMain/HomePageMain.js
@@ -2,11 +2,19 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 import Head from '../../../../_atoms/Head';
+import ResponsiveWrapper from '../../../../_molecules/ResponsiveWrapper/ResponsiveWrapper';
 import { withText } from '../../../../../text/textStore';
-
+import style from './HomePageMain.css';
 
 const HomePageMain = ({ Text: text }) => (
-  <Head title={text({ path: 'defaultTitle' })} />
+  <>
+    <Head title={text({ path: 'defaultTitle' })} />
+    <ResponsiveWrapper>
+      <div className={style.temp}>
+        hello
+      </div>
+    </ResponsiveWrapper>
+  </>
 );
 
 HomePageMain.propTypes = {
