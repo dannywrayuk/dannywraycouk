47441501f984f12423d363a1e008df80da935d29
dannywrayuk <dannywray97@gmail.com>
2020-02-29 05:52:33 +0000

    added rollback feature

--- aws/invalidate_index.js
+++ aws/invalidate_index.js
@@ -4,6 +4,6 @@ const aws = new awsCli.Aws();
 
 const DIST_NAME = 'E3CI2I43CYNJR9';
 
-aws.command(`cloudfront create-invalidation --distribution-id ${DIST_NAME} --paths /index.html`).then((data) => {
+aws.command(`cloudfront create-invalidation --distribution-id ${DIST_NAME} --paths / /index.html`).then((data) => {
   console.log(data.raw);
 });
