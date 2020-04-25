aa63bd95ab7e85c4fc30be208389c6bf6a7af99b
dannywrayuk <dannywray97@gmail.com>
2020-04-25 01:14:48 +0100

    removed unused style

--- src/components/environments/Physics/presentation/PhysicsPost/PhysicsPost.style.js
+++ src/components/environments/Physics/presentation/PhysicsPost/PhysicsPost.style.js
@@ -2,11 +2,6 @@ import styled from 'styled-components';
 
 import Paragraph from '../../../../styled/Paragraph';
 
-export const Wrapper = styled.div`
-  width: 100%;
-`;
-
-
 export const Date = styled(Paragraph)`
   font-style: italic;
 `;
