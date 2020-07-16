43a532ad4e4de7c3735802343e439c6d6cafd07d
dannywrayuk <dannywray97@gmail.com>
2020-07-16 15:09:45 +0100

    fix camelcase error

--- src/components/environments/Post/content/Posts.js
+++ src/components/environments/Post/content/Posts.js
@@ -1,5 +1,5 @@
-import n_body_simulations_using_the_particle_mesh_method from '../../../../text/Posts/Blog/n_body_simulations_using_the_particle_mesh_method.md';
 /* eslint-disable camelcase */
+import n_body_simulations_using_the_particle_mesh_method from '../../../../text/Posts/Blog/n_body_simulations_using_the_particle_mesh_method.md';
 import pictures_with_primes from '../../../../text/Posts/Blog/pictures_with_primes.md';
 
 export {
