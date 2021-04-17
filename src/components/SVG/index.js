7ba1b3186a35b265e09e07652b3c5ab5a1fd00e0
Danny Wray <dannywray97@gmail.com>
2021-04-17 23:05:40 +0100

    🎨 Add SVG React Components

---
+++ src/components/SVG/index.js
@@ -0,0 +1,6 @@
+import { GithubSVG } from "./Github";
+import { LinkedinSVG } from "./Linkedin";
+import { LogoSVG } from "./Logo";
+import { SearchSVG } from "./Search";
+import { TwitterSVG } from "./Twitter";
+export { GithubSVG, LinkedinSVG, LogoSVG, SearchSVG, TwitterSVG };
