c33984386ffad3e6e4218dcf33f029f4a9e67d29
Danny Wray <dannywray97@gmail.com>
2021-06-09 01:27:52 +0100

    🐛 Fix cleaning on root url

--- infrastructure/clean-url-handler.js
+++ infrastructure/clean-url-handler.js
@@ -1,12 +1,12 @@
 function handler(event) {
   var request = event.request;
   var uri = request.uri;
-
-  if (uri.endsWith("/") && uri.length > 1) {
-    request.uri = request.uri.split(0, -2) + ".html";
-  } else if (!uri.includes(".")) {
-    request.uri += ".html";
-  }
+  if (uri !== "/")
+    if (uri.endsWith("/")) {
+      request.uri = uri.slice(0, -1) + ".html";
+    } else if (!uri.includes(".")) {
+      request.uri += ".html";
+    }
 
   return request;
 }
