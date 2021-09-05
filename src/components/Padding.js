9df1fb79877e11c4d6955364eb5097cd838c80b5
Danny Wray <dannywray97@gmail.com>
2021-09-05 22:50:26 +0100

    🖥 Fix scrollbar issue on windows

--- src/components/Padding.js
+++ src/components/Padding.js
@@ -1,7 +1,7 @@
 import { Flex, Box } from "@chakra-ui/react";
 
 const Padding = ({ children }) => (
-  <Flex justifyContent="center" w="100vw">
+  <Flex justifyContent="center" w="100%">
     <Box
       maxW={1200}
       minW={300}
