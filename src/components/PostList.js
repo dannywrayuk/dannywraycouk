ab91a5ed5f252f72bd82764e46d863985d03824b
Danny Wray <dannywray97@gmail.com>
2021-10-24 23:08:00 +0100

    ✨ Sort by date

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,24 +1,32 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
+import { SubTitle } from "./Text";
+
 export const PostList = ({ posts }) => {
   let currentYear = 0;
   return (
-    <Flex flexDirection="column" gridRowGap="20px">
-      {posts.map(({ title, date }) => {
-        let day, month, year, showYear;
-        if (date) {
-          [, day, month, year] = date.match(/(\d\d) (\w\w\w) (\d\d\d\d)/);
-          showYear = currentYear < year;
-        }
-        return (
-          <>
-            {showYear && <div>{year}</div>}
-            <Flex gridColumnGap="30px">
-              <div>{date ? `${month} ${day}` : "---"}</div>
-              <div>{title}</div>
-            </Flex>
-          </>
-        );
-      })}
+    <Flex flexDirection="column" gridRowGap="5px">
+      {posts
+        .sort(
+          (a, b) =>
+            (b.date ? Date.parse(b.date) : 0) -
+            (a.date ? Date.parse(a.date) : 0)
+        )
+        .map(({ title, date }) => {
+          let day, month, year, showYear;
+          if (date) {
+            [, day, month, year] = date.match(/(\d\d) (\w\w\w) (\d\d\d\d)/);
+            showYear = currentYear < year;
+          }
+          return (
+            <>
+              {showYear && <SubTitle py="20px">{year}</SubTitle>}
+              <Flex gridColumnGap="20px" h="100px" alignItems="center">
+                <Box minW="60px">{date ? `${month} ${day}` : "-"}</Box>
+                <Box>{title}</Box>
+              </Flex>
+            </>
+          );
+        })}
     </Flex>
   );
 };
