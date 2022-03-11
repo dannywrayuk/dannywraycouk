ebe8dca2f8ff5df04f5877c105f3d98fbaf45881
Danny Wray <dannywray97@gmail.com>
2022-03-11 00:55:11 +0000

    Add basic suggestions

--- src/components/CommandPalette.js
+++ src/components/CommandPalette.js
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
 import { md } from "@utils/breakpoints";
 import { createPortal } from "react-dom";
+import { Text } from "./core";
 
 const Overlay = styled.div({
   position: "fixed",
@@ -20,12 +21,12 @@ const BodyWrapper = styled.div({
   top: 0,
   width: "100%",
   overflow: "hidden",
-  borderBottom: "1px solid #00000088",
+  borderBottom: "1px solid #00000055",
   [md]: {
     top: "20%",
     width: "600px",
-    borderRadius: "20px",
-    border: "1px solid #00000088",
+    borderRadius: "15px",
+    border: "1px solid #00000055",
   },
 });
 
@@ -39,9 +40,48 @@ const SearchInput = styled.input({
   fontSize: "1.25rem",
 });
 
+const SuggestionsWrapper = styled.div({
+  maxHeight: "400px",
+  overflowY: "scroll",
+});
+
+const Section = styled.div({
+  padding: "0 30px",
+  height: "30px",
+  display: "flex",
+  alignItems: "center",
+  backgroundColor: "grey",
+});
+
+const Item = styled.div({
+  padding: "0 30px",
+  height: "50px",
+  display: "flex",
+  alignItems: "center",
+});
+
+const Suggestions = () => (
+  <SuggestionsWrapper>
+    <Section>
+      <Text>Navigation</Text>
+    </Section>
+    <Item>Toggle Colour Mode</Item>
+    <Item>Toggle Colour Mode</Item>
+    <Section>Navigation</Section>
+    <Item>Toggle Colour Mode</Item>
+    <Item>Toggle Colour Mode</Item>
+    <Section>Navigation</Section>
+    <Item>Toggle Colour Mode</Item>
+    <Item>Toggle Colour Mode</Item>
+    <Item>Toggle Colour Mode</Item>
+    <Item>Toggle Colour Mode</Item>
+  </SuggestionsWrapper>
+);
+
 const Body = () => (
   <BodyWrapper>
     <SearchInput placeholder="Type here to find things" />
+    <Suggestions />
   </BodyWrapper>
 );
 
