894cc7217fc63128c201468a5b571f735934f1e3
Danny Wray <dannywray97@gmail.com>
2022-05-21 01:41:14 +0100

    general improvements

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,38 +1,34 @@
-import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 import GoogleFonts from "@utils/GoogleFonts";
 
-export default class Document extends NextDocument {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link href="/globals.css" rel="stylesheet" />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `document.documentElement.setAttribute('colorMode', 'light');`,
-            }}
-          />
-          <GoogleFonts
-            fonts={{
-              Sora: { weights: [400, 600, 700] },
-              Montserrat: { weights: [400, 600, 700] },
-              Inter: { weights: [400, 600, 700] },
-              Karla: { weights: [400, 600, 700] },
-            }}
-          />
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/katex@0.13.3/dist/katex.min.css"
-            integrity="sha384-ThssJ7YtjywV52Gj4JE/1SQEDoMEckXyhkFVwaf4nDSm5OBlXeedVYjuuUd0Yua+"
-            crossOrigin="anonymous"
-          />
-        </Head>
-        <body>
-          <Main />
-          <div id="portal" />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
+export default () => (
+  <Html lang="en">
+    <Head>
+      <link href="/globals.css" rel="stylesheet" />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `document.documentElement.setAttribute('colorMode', 'light');`,
+        }}
+      />
+      <GoogleFonts
+        fonts={{
+          Sora: { weights: [400, 600, 700] },
+          Montserrat: { weights: [400, 600, 700] },
+          Inter: { weights: [400, 600, 700] },
+          Karla: { weights: [400, 600, 700] },
+        }}
+      />
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/katex@0.13.3/dist/katex.min.css"
+        integrity="sha384-ThssJ7YtjywV52Gj4JE/1SQEDoMEckXyhkFVwaf4nDSm5OBlXeedVYjuuUd0Yua+"
+        crossOrigin="anonymous"
+      />
+    </Head>
+    <body>
+      <Main />
+      <div id="portal" />
+      <NextScript />
+    </body>
+  </Html>
+);
