2386769e9357add920bfc4ed5e5e32b1112080d5
Danny Wray <dannywray97@gmail.com>
2022-06-14 03:05:01 +0100

    add background to pdflink

--- src/components/RouteLayout/layouts/ProjectPdf.js
+++ src/components/RouteLayout/layouts/ProjectPdf.js
@@ -7,15 +7,24 @@ const PdfWrapper = styled.div({
   display: "flex",
   flexDirection: "column",
   justifyContent: "space-between",
-  alignItems: "center",
+  alignItems: "stretch",
   backgroundColor: "var(--color-bg-muted)",
   padding: "10px 20px",
   margin: "20px -10px 0",
   borderRadius: "0.5em",
   textAlign: "center",
+  rowGap: "10px",
   [mq.sm]: {
     flexDirection: "row",
+    alignItems: "center",
   },
+});
+
+const PdfLink = styled(Link)({
+  backgroundColor: "var(--color-brand-primary)",
+  color: "black",
+  padding: "5px 10px",
+  borderRadius: "0.5em",
 });
 
 export const ProjectPdf = ({
@@ -30,9 +39,9 @@ export const ProjectPdf = ({
       <Hr />
       <PdfWrapper>
         <Text>The PDF for this project is available here</Text>
-        <Text>
-          <Link href={pdfLink}>View PDF</Link>
-        </Text>
+        <PdfLink href={pdfLink}>
+          <Text>View PDF</Text>
+        </PdfLink>
       </PdfWrapper>
     </Flex>
     <Flex column rg="10px" className={className}>
