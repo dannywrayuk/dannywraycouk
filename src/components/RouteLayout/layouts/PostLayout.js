7a1a795b80c181b84c27968ac7a390b46fb81911
Danny Wray <dannywray97@gmail.com>
2022-06-22 02:46:30 +0100

    center text on posts

--- src/components/RouteLayout/layouts/PostLayout.js
+++ src/components/RouteLayout/layouts/PostLayout.js
@@ -1,17 +1,39 @@
-import { Flex, Heading, Hr, Text } from "@components/core";
-import { Layout, LayoutCore, SectionSpacing } from "@components/Layout";
+import { Flex, Heading, Text } from "@components/core";
+import { Layout } from "@components/Layout";
+import styled from "@emotion/styled";
+import { mq } from "@utils/breakpoints";
+
+const TitleWrapper = styled(Flex)({
+  [mq.sm]: {
+    textAlign: "center",
+  },
+});
 
 export const PostLayout = ({
   children,
-  data: { title, abstract },
+  data: { title, abstract, tags, hideTags, dateString, hideDate },
   className,
 }) => (
   <Layout>
-    <Flex column>
-      <Heading mt={60}>{title}</Heading>
-      <Text>{abstract}</Text>
-      <Hr />
-    </Flex>
+    <TitleWrapper my={60} column rg={30}>
+      {!hideDate && dateString && (
+        <Text color="var(--color-fg-link)">{dateString}</Text>
+      )}
+      <Heading fontSize="2.25rem">{title}</Heading>
+      {!hideTags && tags && (
+        <Text color="var(--color-fg-link)">{tags.join(", ")}</Text>
+      )}
+      {abstract && (
+        <Heading
+          as="h2"
+          size="md"
+          weight="normal"
+          color="var(--color-fg-muted)"
+        >
+          {abstract}
+        </Heading>
+      )}
+    </TitleWrapper>
     <Flex column rg="10px" className={className}>
       {children}
     </Flex>
